8ace5bdbae998b1b37ee9caf777f5c2056b0ce67
gaoxiong04 <gaoxiong04@maoyan.com>
2024-09-06 15:00:26 +0800

    。

---
+++ CadenTool/ViewModels/CadenBaseControlViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadenTool.ViewModels
+{
+    class CadenBaseControlViewModel : BindableBase
+    {
+        public string BlogTitle => "小高的知识库";
+        public string BlogSubTitle => "she is not following you";
+
+
+    }
+
+}
---
+++ CadenTool/ViewModels/CadenBaseGeneControlViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadenTool.ViewModels
+{
+    [RegionMemberLifetime(KeepAlive = false)]
+    class CadenBaseGeneControlViewModel : BindableBase
+    {
+        public DelegateCommand PullOriginDataFromYQ { get; private set; }
+        public DelegateCommand GeneLocalBlogData { get; private set; }
+        public DelegateCommand GeneWpBlogData { get; private set; }
+        public CadenBaseGeneControlViewModel()
+        {
+            // 初始化命令，并指定执行方法
+            PullOriginDataFromYQ = new DelegateCommand(ExecutePullOriginDataFromYQ);
+            GeneLocalBlogData = new DelegateCommand(ExecuteGeneLocalBlogData);
+            GeneWpBlogData = new DelegateCommand(ExecuteGeneWpBlogData);
+        }
+
+        // 执行方法
+        private void ExecutePullOriginDataFromYQ()
+        {
+            // 处理拉取语雀云端数据的逻辑
+            // 例如调用服务从云端拉取数据
+        }
+
+        private void ExecuteGeneLocalBlogData()
+        {
+            // 处理生成本地博客的逻辑
+        }
+
+        private void ExecuteGeneWpBlogData()
+        {
+            // 处理生成本地博客的逻辑
+        }
+    }
+}
